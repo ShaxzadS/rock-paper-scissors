b89572667e2323db916f9266b615a0db365b4e54
Shaxzad Nizamdinov <shaxzadnizamdinov@gmail.com>
2024-10-25 21:57:23 +0500

    Описание изменений

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>c4422e2c</title>
+<title>c4422e2c</title>
+<?php require_once "bootstrap.php"; ?>
 </head>
 <body>
-    <h1>Welcome to the Rock Paper Scissors Game!</h1>
-    <a href="login.php">Please Log In</a>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
 </body>
-</html>
-
